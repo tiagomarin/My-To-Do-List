74c9169927415d112e8bce3415b2a05cb81c06cb
Tiago Marin <tiago.lelinski@gmail.com>
2022-07-21 22:54:18 -0300

    fix tests of remove task

--- __tests__/add.test.js
+++ __tests__/add.test.js
@@ -7,8 +7,39 @@ import deleteTask from "../src/modules/deleteTask";
 import getArr from "../src/modules/getDataFromLocalStorage";
 import saveInLocalStorage from "../src/modules/saveAtLocalStorage.js";
 
+// add or remove exactly one <li> element to/from the list in the DOM.
 
 describe('add & remove', () => {
+
+  //set up environment
+  document.body.innerHTML = `<main>+
+  <section id="task-list">+
+    <div id="list-title">+
+      <h2>Today's To Do</h2>+
+      <span><i class="fa-solid fa-arrows-rotate"></i></span>+
+    </div>+
+      <input id="task-text" type="text" placeholder="Add your task..." required>+
+      <button id="add-task-btn" type="submit"><i class="fa-solid fa-plus"></i></button>+
+    <div class="check-all">+
+      <input  type="checkbox">+
+      <label>SELECT ALL</label>+
+    </div>+
+    <ul id="list-placeholder"></ul>+
+    <button id="clear-btn">Clear all completed</button>+
+  </section>+
+</main>`;
+
+  // save fake values to local storage
+  const taskListArr = getArr();
+  for (let i = 1; i <= 10; i++) {
+    let task = { Description: `fake${i}`, Completed: false, Index: i }
+    taskListArr.push(task);
+  }
+  saveInLocalStorage(taskListArr);
+  const initialArray = taskListArr;
+  let arrFromStorage = getArr();
+
+  // start add tests
   describe('add task', () => {
     test("new item created has correct Description value", () => {
       expect(createTask("pet the dog", 3).Description).toEqual("pet the dog");
@@ -31,52 +62,29 @@ describe('add & remove', () => {
     })
   })
 
-
-
-
   describe('remove task', () => {
+
+
+    // begin of tests
     test("item not removed", () => {
+      const arrLength = arrFromStorage.length; //store length to compare later
+      deleteTask(2);
+      arrFromStorage = getArr();
+      expect(arrFromStorage.length).toEqual(arrLength - 1)
+    });
 
-      document.body.innerHTML = `<main>+
-      <section id="task-list">+
-        <div id="list-title">+
-          <h2>Today's To Do</h2>+
-          <span><i class="fa-solid fa-arrows-rotate"></i></span>+
-        </div>+
-          <input id="task-text" type="text" placeholder="Add your task..." required>+
-          <button id="add-task-btn" type="submit"><i class="fa-solid fa-plus"></i></button>+
-        <div class="check-all">+
-          <input  type="checkbox">+
-          <label>SELECT ALL</label>+
-        </div>+
-        <ul id="list-placeholder"></ul>+
-        <button id="clear-btn">Clear all completed</button>+
-      </section>+
-    </main>`;
+    test("correct item was removed", () => {
+      const itemToRemove = arrFromStorage[2].Description; //store value of Description to compare later
+      deleteTask(2);
+      arrFromStorage = getArr();
+      expect(arrFromStorage[2].Description).not.toEqual(itemToRemove)
+    });
 
-      console.log(document)
-      const one = document.getElementById('task-text').value = "task1";
-      const taskListArr = getArr();
-      const index = (taskListArr.length + 1);
-      const newTask = createTask(one, index);
-      taskListArr.push(newTask);
-      console.log(taskListArr);
-      saveInLocalStorage(taskListArr);
-
-      document.getElementById('task-text').value = "task2";
-      document.getElementById('add-task-btn').click();
-      const taskListArr2 = getArr();
-      console.log(taskListArr2);
-      document.getElementById('task-text').value = "task3";
-      // document.getElementById('add-task-btn').click();
-      document.getElementById('task-text').value = "task3";
-      // document.getElementById('add-task-btn').click();
-
-      let arr = getArr();
-      const arrLength = arr.length;
-      deleteTask(0);
-      expect(0).toEqual(0)
-    })
-  })
-
+    test("property Index is updated", () => {
+      const index = arrFromStorage[2].Index; //store value of property Index to compare later
+      deleteTask(2);
+      arrFromStorage = getArr();
+      expect(arrFromStorage[2].Index).toEqual(index)
+    });
+  });
 });
